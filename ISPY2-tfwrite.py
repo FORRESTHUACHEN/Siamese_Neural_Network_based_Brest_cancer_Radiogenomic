b7c86cf93d7b469f51f4115545d50111c06f884e
Junhua Chen <48844820+FORRESTHUACHEN@users.noreply.github.com>
2024-01-22 17:37:05 +0800

    Update ISPY2-tfwrite.py

--- ISPY2-tfwrite.py
+++ ISPY2-tfwrite.py
@@ -11,64 +11,34 @@ tf.disable_v2_behavior()
 from itertools import chain
 import numpy as np
 import pandas as pd
-from combat.pycombat import pycombat
 
-label=pd.read_excel('D:/Datasets/ISPY1-Tumor-SEG-Radiomics/Labels.xlsx')
 
-Radiomics_feature=pd.read_excel('D:/Datasets/ISPY1-Tumor-SEG-Radiomics/WithNormRadiomicsSummary.xlsx')
+label=pd.read_excel('D:/DataSets/ACRIN-6698/Classifier-LSTM/ISPY2ClinicalLabel.xlsx')
 
-Deep_feature=pd.read_excel('D:/Datasets/ISPY1-Tumor-SEG-Radiomics/WithNormDeepfeaturesSummary.xlsx')
+Radiomics_feature=pd.read_csv('D:/DataSets/ACRIN-6698/Classifier-LSTM/ISPY2CoreSegmentationNormalizedRadiomicsSummary.csv')
+
+Deep_feature=pd.read_csv('D:/DataSets/ACRIN-6698/Classifier-LSTM/ISPY2CoreDeepNormalizedFeatureSummary.csv')
 
 Radiomics_feature_group=Radiomics_feature.groupby(['Patient_ID'])
 
 Deep_feature_group=Deep_feature.groupby(['Patient_ID'])
 
 Radiomics_label_unique=list(Radiomics_feature.loc[:,'Patient_ID'].unique())
-manu_label=pd.read_excel('D:/Datasets/Data Recovered/DataSets/ACRIN-6698/Codes/manufacturer_info.xlsx')
 
+# Deep_feature_unique=list(Deep_feature.loc[:,'Patient_ID'].unique())
 
+# Common_samples=list(set(Radiomics_label_unique)&set(Deep_feature_unique))
 
-# writer1= tf.python_io.TFRecordWriter("D:/Datasets/Data Recovered/Rec Core Fle/SubtypesDecodingCore_featureTrain_HER2_combat10.tfrecords") 
-# writer2= tf.python_io.TFRecordWriter("D:/Datasets/Data Recovered/Rec Core Fle/SubtypesDecodingCore_featureTest_HER2_combat10.tfrecords")
+writer1= tf.python_io.TFRecordWriter("D:/DataSets/ACRIN-6698/Classifier-LSTM/SubtypesDecodingCore_featureTrain2.tfrecords") 
+writer2= tf.python_io.TFRecordWriter("D:/DataSets/ACRIN-6698/Classifier-LSTM/SubtypesDecodingCore_featureTest2.tfrecords")
 
-writer1= tf.python_io.TFRecordWriter("D:/Datasets/ISPY1-Tumor-SEG-Radiomics/externalTest_Her2_combat.tfrecords") 
-writer2= tf.python_io.TFRecordWriter("D:/Datasets/ISPY1-Tumor-SEG-Radiomics/externalTrain_Her2_combat.tfrecords")
  
 
-index=list(range(161))
+index=list(range(380))
 np.random.shuffle(index)
 
 count1=0
 count2=0
-# # deepfeature_label=np.zeros(len(Deep_feature))
-# # for deep_label in range(len(Deep_feature)):
-# #     deepfeature_label[deep_label]=manu_label[manu_label['Patient ID']==Deep_feature.iloc[deep_label][0]].iloc[:,1]
-    
-# # deepfeature_value=Deep_feature.iloc[:,2:]
-
-# # #deepfeautre_label_list=list(deepfeature_label)
-
-
-# # deepfeature_corrected = pycombat(deepfeature_value.T,deepfeature_label.T)
-
-# # deepfeature_corrected =deepfeature_corrected.T
-
-# # Deep_feature.iloc[:,2:]=deepfeature_corrected
-
-# # radiomics_label=np.zeros(len(Radiomics_feature))
-
-# # for radio_label in range(len(Radiomics_feature)):
-# #     radiomics_label[radio_label]=manu_label[manu_label['Patient ID']==Radiomics_feature.iloc[radio_label][0]].iloc[:,1]
-# # radiomics_value=Radiomics_feature.iloc[:,2:]
-
-# # radiomics_corrected=pycombat(radiomics_value.T,radiomics_label.T)
-
-# # radiomics_corrected=radiomics_corrected.T
-
-# # Radiomics_feature.iloc[:,2:]=radiomics_corrected
-
-# # Radiomics_feature.to_csv('D:/Datasets/Data Recovered/DataSets/ACRIN-6698/Classifier-LSTM/combatcorrectedRadiomics.csv')
-# # Deep_feature.to_csv('D:/Datasets/Data Recovered/DataSets/ACRIN-6698/Classifier-LSTM/combatcorrectedDeepfeatures.csv')
 
 for i in range(len(Radiomics_label_unique)):
     Radiomics_feature_group_sample=Radiomics_feature_group.get_group(Radiomics_label_unique[i])
@@ -78,18 +48,18 @@ for i in range(len(Radiomics_label_unique)):
     Radiomics_feature_group_sample_data=Radiomics_feature_group_sample_data
     Deep_feature_group_sample_data=np.array(Deep_feature_group_sample.iloc[:,2:],dtype=np.float32)
     Deep_feature_group_sample_data=Deep_feature_group_sample_data
-    if sum(label.Patient_ID==int(Radiomics_label_unique[i]))>0:
+    if sum(label.Patient_ID==int(Radiomics_label_unique[i][-6:]))>0:
         
-        Sample_label=label.loc[label.Patient_ID==int(Radiomics_label_unique[i]),:]
+        Sample_label=label.loc[label.Patient_ID==int(Radiomics_label_unique[i][-6:]),:]
         
-        Sample_label_value=abs(int(Sample_label.iloc[0,2])-1)
+        Sample_label_value=int(Sample_label.iloc[0,1])
         
-        # if Sample_label_value>1:
-        #     Sample_label_value=1
+        if Sample_label_value>1:
+            Sample_label_value=1
         Sample_label_value=np.array(Sample_label_value,dtype=np.int32)
 
         
-        #print(Sample_label_value)
+        print(Sample_label_value)
         RealValidatedPhases=np.array(Deep_feature_group_sample_data.shape[0],dtype=np.int32)
     
         if Radiomics_feature_group_sample_data.shape[0]<9:
@@ -112,15 +82,13 @@ for i in range(len(Radiomics_label_unique)):
             'Label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_towrite]))
             }))
     
-        if i in index[0:161]:
+        if i in index[0:310]:
             count1=count1+1
             writer1.write(example.SerializeToString())  
-            print(Radiomics_feature_group_sample_data.shape)
-            print(Deep_feature_group_sample_data.shape)
-            #print(Sample_label_value)
+            print(Sample_label_value)
         else:
             count2=count2+1
             writer2.write(example.SerializeToString())  
-            #print(Sample_label_value)
+        print(Sample_label_value)
 writer1.close()
 writer2.close()
